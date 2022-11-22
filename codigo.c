cdd27e6d807b3bef5c1614255b70ad21bac091b4
enrique-mac <carvalhoenrique33@gmail.com>
2022-11-22 13:27:58 -0300

    Atualização Código

--- codigo.c
+++ codigo.c
@@ -1,6 +1,45 @@
-#include <stdio.h> // inclusao de biblioteca
+/*
+Integrantes:
+Nome Completo: Enrique de Lima Carvalho Matrícula: UC22103250 Curso: Engenharia de Software
+Nome Completo: Emanuelly de Souza Portela Matrícula: UC22200313 Curso: Análise e Desenvolvimento de Sistemas
+Nome Completo: Gabrielly de Souza Portela Matrícula: UC22200274 Curso: Análise e Desenvolvimento de Sistemas
+
+Um cinema de Brasília está interessado em descobrir o perfil das pessoas
+que frequentam suas sessões de filmes e, para isso, necessita analisar as informações
+prestadas. Devem ser solicitados a quantidade de sessões que serão realizadas e, para cada
+sessão, o nome do filme, a quantidade de pessoas que assistiram o filme, assim como, o sexo, a
+idade e se foi pago inteira ou meia entrada. Para isso, algumas validações devem ser realizadas:
+1. Solicite a quantidade de sessões, aceitando exatamente 2 (duas) sessões. (0,4 ponto)
+2. Solicite a quantidade de pessoas que assistiram ao filme, não aceitando valor inferior
+a 10 (dez). (0,4 ponto)
+3. Para cada pessoa que assistiu ao filme deverá ser solicitado e validado o sexo (M -
+masculino, F - Feminino) e a idade (de 3 a 100 anos) (0,4 ponto).
+4. Apresentar ao usuário apenas as opções Inteira e Meia (0,4 ponto), e somar o valor
+arrecadado na sessão, considerando que a inteira custa R$50,00 e a meia R$25,00 (0,4
+ponto).
+Após a leitura dos dados do filme, deverá ser apresentado:
+5. o nome do filme, a quantidade de pessoas do sexo feminino e a quantidade de
+pessoas do sexo masculino que assistiram ao filme (0,4 ponto).
+6. a quantidade de pessoas, seguindo a classificação por idade indicada abaixo. (0,4 ponto).
+Idade Classificação
+|de 3 até 13 anos - Crianças|
+|de 14 até 17 anos - Adolescentes|
+|de 18 até 64 anos - Adultos|
+|de 65 até 100 anos - Idosos|
+7. Depois da leitura dos dados de cada sessão, deverá ser mostrada a quantidade de
+pessoas maiores de idade (idade maior ou igual a 18 anos) do sexo masculino e a
+quantidade de pessoas maiores de idade do sexo feminino que estiveram presentes. (0,4
+ponto).
+8. Informar o valor total arrecadado em cada sessão e informar se houve mais pagamento
+de inteira que de meia entrada. (0,4 ponto).
+9. Dentro do contexto da atividade, pense em uma nova funcionalidade e implemente. (0,4
+ponto).
+*/
+#include <stdio.h> // inclusao de biblioteca padrao
+#include <locale.h> // inclusao de biblioteca para tradução
 
 int main(){ // funcao main
+    setlocale(LC_ALL, "Portuguese");
 
 
     
