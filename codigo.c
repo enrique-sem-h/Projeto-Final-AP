ec7b61a2bc98eff40e956ce660f5897890b3259a
emanuelly-portela <emanuellyportela@gmail.com>
2022-11-30 20:16:09 -0300

    Ajuste do codigo e continuação do projeto

--- codigo.c
+++ codigo.c
@@ -1,125 +1,198 @@
 /*
 Integrantes:
-Nome Completo: Enrique de Lima Carvalho Matr√≠cula: UC22103250 Curso: Engenharia de Software
-Nome Completo: Emanuelly de Souza Portela Matr√≠cula: UC22200313 Curso: An√°lise e Desenvolvimento de Sistemas
-Nome Completo: Gabrielly de Souza Portela Matr√≠cula: UC22200274 Curso: An√°lise e Desenvolvimento de Sistemas
+Nome Completo: Enrique de Lima Carvalho MatrÌcula: UC22103250 Curso: Engenharia de Software
+Nome Completo: Emanuelly de Souza Portela MatrÌcula: UC22200313 Curso: An·lise e Desenvolvimento de Sistemas
+Nome Completo: Gabrielly de Souza Portela MatrÌcula: UC22200274 Curso: An·lise e Desenvolvimento de Sistemas
 
-Um cinema de Bras√≠lia est√° interessado em descobrir o perfil das pessoas
-que frequentam suas sess√µes de filmes e, para isso, necessita analisar as informa√ß√µes
-prestadas. Devem ser solicitados a quantidade de sess√µes que ser√£o realizadas e, para cada
-sess√£o, o nome do filme, a quantidade de pessoas que assistiram o filme, assim como, o sexo, a
-idade e se foi pago inteira ou meia entrada. Para isso, algumas valida√ß√µes devem ser realizadas:
-1. Solicite a quantidade de sess√µes, aceitando exatamente 2 (duas) sess√µes. (0,4 ponto)
-2. Solicite a quantidade de pessoas que assistiram ao filme, n√£o aceitando valor inferior
+Um cinema de BrasÌlia est· interessado em descobrir o perfil das pessoas
+que frequentam suas sessıes de filmes e, para isso, necessita analisar as informaÁıes
+prestadas. Devem ser solicitados a quantidade de sessıes que ser„o realizadas e, para cada
+sess„o, o nome do filme, a quantidade de pessoas que assistiram o filme, assim como, o sexo, a
+idade e se foi pago inteira ou meia entrada. Para isso, algumas validaÁıes devem ser realizadas:
+1. Solicite a quantidade de sessıes, aceitando exatamente 2 (duas) sessıes. (0,4 ponto)
+2. Solicite a quantidade de pessoas que assistiram ao filme, n„o aceitando valor inferior
 a 10 (dez). (0,4 ponto)
-3. Para cada pessoa que assistiu ao filme dever√° ser solicitado e validado o sexo (M -
+3. Para cada pessoa que assistiu ao filme dever· ser solicitado e validado o sexo (M -
 masculino, F - Feminino) e a idade (de 3 a 100 anos) (0,4 ponto).
-4. Apresentar ao usu√°rio apenas as op√ß√µes Inteira e Meia (0,4 ponto), e somar o valor
-arrecadado na sess√£o, considerando que a inteira custa R$50,00 e a meia R$25,00 (0,4
+4. Apresentar ao usu·rio apenas as opÁıes Inteira e Meia (0,4 ponto), e somar o valor
+arrecadado na sess„o, considerando que a inteira custa R$50,00 e a meia R$25,00 (0,4
 ponto).
-Ap√≥s a leitura dos dados do filme, dever√° ser apresentado:
+ApÛs a leitura dos dados do filme, dever· ser apresentado:
 5. o nome do filme, a quantidade de pessoas do sexo feminino e a quantidade de
 pessoas do sexo masculino que assistiram ao filme (0,4 ponto).
-6. a quantidade de pessoas, seguindo a classifica√ß√£o por idade indicada abaixo. (0,4 ponto).
-Idade Classifica√ß√£o
-|de 3 at√© 13 anos - Crian√ßas|
-|de 14 at√© 17 anos - Adolescentes|
-|de 18 at√© 64 anos - Adultos|
-|de 65 at√© 100 anos - Idosos|
-7. Depois da leitura dos dados de cada sess√£o, dever√° ser mostrada a quantidade de
+6. a quantidade de pessoas, seguindo a classificaÁ„o por idade indicada abaixo. (0,4 ponto).
+Idade ClassificaÁ„o
+|de 3 atÈ 13 anos - CrianÁas|
+|de 14 atÈ 17 anos - Adolescentes|
+|de 18 atÈ 64 anos - Adultos|
+|de 65 atÈ 100 anos - Idosos|
+7. Depois da leitura dos dados de cada sess„o, dever· ser mostrada a quantidade de
 pessoas maiores de idade (idade maior ou igual a 18 anos) do sexo masculino e a
 quantidade de pessoas maiores de idade do sexo feminino que estiveram presentes. (0,4
 ponto).
-8. Informar o valor total arrecadado em cada sess√£o e informar se houve mais pagamento
+8. Informar o valor total arrecadado em cada sess„o e informar se houve mais pagamento
 de inteira que de meia entrada. (0,4 ponto).
 9. Dentro do contexto da atividade, pense em uma nova funcionalidade e implemente. (0,4
 ponto).
 */
-#include <stdio.h> // inclusao de biblioteca padrao
-#include <locale.h> // inclusao de biblioteca para tradu√ß√£o
+#include <stdio.h> // inclus„o de biblioteca padrao
+#include <locale.h> // inclus„o de biblioteca para traduÁ„o
+#include <stdlib.h> // inclus„o de biblioteca para o comando clear
+#define SALA 90 // capacidade da sala - quantidade de ingressos para aquela sessao
+#define SESSOES 2 // quantidade de sessıes aceitas
+#define INTEIRA 50.00 // valor do ingresso - inteira
+#define MEIA 25.00 // valor do ingresso - meia
+#define PREMIUM 120.00 // valor do ingresso - Premium
 
 int main(){ // funcao main
-    setlocale(LC_ALL, "Portuguese");
-    
-   	char nomeFilme1[30], nomeFilme2[30], opcaoIngresso;//"s" para salvar a qnt de pessoas
-	int  i, qntsessao, qntpessoas, valorSessao=0, validacao = 1;//valor sessao inicial ser· 0, mas sempre q alguem entrar com o valor ele vai somando as entradas
 
-do{
-	printf("Digite a quantidade de sessıes: ");//quest„o 1
-	scanf("%i", &qntsessao);
-	
-	if(qntsessao==2){
-			validacao = 1;//
-			printf("Sess„o 1:\n");
-			printf("Digite o nome do filme: ");// enuciado
-			scanf("%s", &nomeFilme1[30]);
-			
-			}else{
-	printf("Sess„o inv·lida.\n");
-	validacao = 0;
-	}//fim else
-}while(validacao != 1);//fim "do"
-			
-			do{	
-			printf("Quantidade de pessoas: ");// enuciado
-			scanf("%i", &qntpessoas);
-			if(qntpessoas<10){
-				printf("Quantidade inv·lida! (MinÌmo 10) \n");
-							}
-		}while(qntpessoas < 10);
-				
-char sexo[qntpessoas];//declaraÁ„o da variavel nesse momento, pois È somente aqui que vamos declarar a qnt de pessoas
-int idade[qntpessoas];		
-		for(i=0;i<qntpessoas; i++){
-		do{
-			printf("Digite o sexo, F-Feminino ou M-Masculino:\n");// quest„o 3
-			scanf(" %c", &sexo[i]);
-			fflush(stdin);
-				
-				if(sexo[i]!='F'&& sexo[i]!='M'&&sexo[i]!='f'&& sexo[i]!='m'){
-				printf("Sexo inv·lido!Digite novamente.\n");
-			}		
-		}while(sexo[i]!='F'&& sexo[i]!='M'&&sexo[i]!='f'&& sexo[i]!='m');	
-		do{
-			printf("Digite a idade (entre 03 e 100 anos):");
-			scanf("%i", &idade[i]);
-			fflush(stdin);
-			
-			if(idade[i]<3 || idade[i]>100){
-			printf("Idade inv·lida! Digite novamente.");	
-			}	
-			
-		}while(idade[i]<3 || idade[i]>100);//queset„o 4
-		repeticao:
-		printf("Digite a opÁ„o deseja: (M) para meia entrada = R$25,00 ou (I) para inteira = R$50,00:");
-		scanf("%c", &opcaoIngresso);
-		fflush(stdin);
-				
-		switch (opcaoIngresso){
-			case 'M':
-				valorSessao=valorSessao+25;
-				break;
-			case 'I':
-				valorSessao=valorSessao+50;
-				break;
-			default: 
-			printf("OpÁ„o inv·lida!Digite novamente.\n");
-			goto repeticao;//funÁ„o utilizada para repetiÁ„o, looping, n„o precisa q fale pra ele ate qnd deve repetir
-		}//fim do switch case
-		}//fim do for
-		printf("Valor arrecadado na sess„o: %i\n", valorSessao);//somar os valores gravados das sessoes
-		
-		printf("Nome do filme:\n");
-		scanf("%c", &nomeFilme1[30]);
-		fflush(stdin);
-		
-		printf("A quantidade de pessoas do sexo F-feminino È: ");
-		scanf("%c", &sexo[qntpessoas]);
-		fflush(stdin);
-		
-		printf("A quantidade de pessoas do sexo M-masculino È: ");
-		scanf("%c", &sexo[qntpessoas]);
-		fflush(stdin);
+    setlocale(LC_ALL, "Portuguese");// comando para reconhecer acentuaÁ„o
 
-		    
-}//fim do main
+    int i = 1, j = 1, qntSessao, opcaoIngresso[SALA], qntPessoas, qntIngresso[SESSOES], idade[SALA], parar = 0; // declaraÁ„o de vari·veis - inteiros
+    int qntMulheres[SESSOES], qntHomens[SESSOES], qntCriancas[SESSOES], qntAdolescentes[SESSOES], qntAdultos[SESSOES], qntIdosos[SESSOES]; // declaraÁ„o de vari·veis - inteiros
+    int maioresHomens[SESSOES], maioresMulheres[SESSOES], qntInteiras[SESSOES], qntMeias[SESSOES], qntPremium[SESSOES];
+    char sexo[SALA], recomecar, nomeFilme[2][30]; // declaraÁ„o de vari·veis - caracteres
+
+    do{
+        system("cls"); // comando para limpar o console
+    printf("Digite a quantidade de sessıes: ");
+    scanf("%d", &qntSessao);
+
+
+
+    if(qntSessao == SESSOES){
+        do{
+            fflush(stdin);
+
+            recomecar = 'c';
+            qntMulheres[i] = 0;
+            qntHomens[i] = 0;
+            qntCriancas[i] = 0;
+            qntAdolescentes[i] = 0;
+            qntAdultos[i] = 0;
+            qntIdosos[i] = 0;
+            maioresHomens[i] = 0;
+            maioresMulheres[i] = 0;
+            qntIngresso[i] = 0;
+            qntInteiras[i] = 0;
+            qntMeias[i] = 0;
+            qntPremium[i] = 0;
+
+                system("cls");
+            printf("Sess„o %d:\n", i);
+            printf("Digite o nome do filme: ");
+            fflush(stdin);
+            scanf(" %[^\n]s", &nomeFilme[i]);
+
+            do{
+                system("cls");
+            printf("Venda de ingresso:\n");
+
+            do{ // SEXO
+            printf("Informe o sexo(M ou F): ");
+            fflush(stdin);
+            scanf(" %c", &sexo[j]);
+
+            if(sexo[j] == 'f' || sexo[j] == 'F'){
+                qntMulheres[i]++;
+            }else if(sexo[j] == 'm' || sexo[j] == 'M')
+                qntHomens[i]++;
+
+            if(!(sexo[j] == 'm' || sexo[j] == 'M' || sexo[j] == 'f' || sexo[j] == 'F'))
+                printf("Sexo inv·lido, tente novamente\n");
+
+            }while(!(sexo[j] == 'm' || sexo[j] == 'M' || sexo[j] == 'f' || sexo[j] == 'F'));
+
+            do{ // IDADE
+            printf("Digite a idade: ");
+            scanf("%d", &idade[j]);
+
+            if(idade[j] >= 3 && idade[j] <= 13)
+                qntCriancas[i]++;
+            else if(idade[j] >= 14 && idade[j] <= 17)
+                qntAdolescentes[i]++;
+            else if(idade[j] >= 18 && idade[j] <= 64)
+                qntAdultos[i]++;
+            else if(idade[j] >= 65 && idade[j] <= 100)
+                qntIdosos[i]++;
+
+            if(idade[j] < 3 || idade[j] > 100)
+                printf("Idade inv·lida, tente novamente\n");
+
+            }while(idade[j] < 3 || idade[j] > 100);
+
+            do{ // TIPO DE INGRESSO
+            printf("\n\t-----ATEN«√O-----\n");
+            printf("\nA OP«√O PREMIUM INCLUI: ENTRADA + PIPOCA MEGA + 2 REFRIGERANTES + CHOCOLATE! \n");
+            printf("\n\t-------------------\n\n");
+            printf("Ingresso:\n1. Meia(R$%.2f)\n2. Inteira(R$%.2f)\n3. Premium(R$%.2f) \n", MEIA, INTEIRA, PREMIUM);
+            scanf("%d", &opcaoIngresso[j]);
+
+            if(opcaoIngresso[j] == 1)
+                qntMeias[i]++;
+            else if(opcaoIngresso[j] == 2)
+                qntInteiras[i]++;
+            else if(opcaoIngresso[j] == 3)
+                qntPremium[i]++;
+
+            if(opcaoIngresso[j] != 1 && opcaoIngresso[j] != 2 && opcaoIngresso[j] != 3)
+                printf("OpÁ„o inv·lida, tente novamente\n");
+            }while(opcaoIngresso[j] != 1 && opcaoIngresso[j] != 2 && opcaoIngresso[j] != 3);
+
+            if((sexo[j] == 'm' || sexo[j] == 'M') && idade[j] >= 18)
+                maioresHomens[i]++;
+            else if((sexo[j] == 'f' || sexo[j] == 'F') && idade[j] >= 18)
+                maioresMulheres[i]++;
+
+            qntIngresso[i]++;
+            j++;
+            parar = 0;
+
+            if(qntIngresso[i] == SALA)
+                printf("\nIngressos para a sess„o esgotados!\n");
+            else if(j == 11 || j > 20){
+                printf("Digite 1 para parar ou 0 para continuar\n");
+                scanf("%d", &parar);
+            }
+            }while(qntIngresso[i] == SALA || parar == 0);
+            i++;;
+        }while(qntSessao >= i);
+    }else{
+        printf("Erro: pressione R para recomeÁar: ");
+        scanf(" %c", &recomecar);
+    }
+    }while(recomecar == 'r' || recomecar == 'R');
+
+    for(i = 1; i <= qntSessao; i++){
+    printf("Sess„o %d:\n", i);
+    printf("Nome do filme: %s\n\n", nomeFilme[i]);
+    printf("Quantidade de pessoas do sexo feminino: %d\n", qntMulheres[i]);
+    printf("Quantidade de pessoas do sexo masculino: %d\n", qntHomens[i]);
+    printf("Quantidade de ingressos vendidos: %d\n\n", qntIngresso[i]);
+
+    printf("\tDados da Sess„o\n");
+    printf("CrianÁas: %d\n", qntCriancas[i]);
+    printf("Adolescentes: %d\n", qntAdolescentes[i]);
+    printf("Adultos: %d\n", qntAdultos[i]);
+    printf("Idosos: %d\n\n", qntIdosos[i]);
+
+    printf("Maiores de idade - Homens: %d\n", maioresHomens[i]);
+    printf("Maiores de idade - Mulheres: %d\n\n", maioresMulheres[i]);
+
+    printf("Ingressos - Inteiras(R$%.2f): %d\n", INTEIRA, qntInteiras[i]);
+    printf("Ingressos - Meias(R$%.2f): %d\n", MEIA, qntMeias[i]);
+    printf("Ingressos - Premuum (R%.2f) : %d\n", PREMIUM, qntPremium[i]);
+    printf("Total arrecadado: (R$%.2f)\n\n", (qntInteiras[i] * INTEIRA) + (qntMeias[i] * MEIA) + (qntPremium[i] * PREMIUM));
+
+    if(qntInteiras[i] > qntMeias[i] && qntInteiras[i] > qntPremium[i])
+        printf("Foram vendidos mais ingressos do tipo: inteira\n");
+    else if(qntMeias[i] > qntInteiras[i] && qntMeias > qntPremium[i])
+        printf("Foram vendidos mais ingressos do tipo: meia\n");
+    else if(qntPremium[i] > qntInteiras[i] && qntPremium[i] > qntMeias)
+        printf("Foram vendidos mais ingressos do tipo: Premium!\n\n");
+    else{
+        printf("Foram vendidos ingressos em quantidades iguais!");
+    }
+    }
+    return 0;
+}
